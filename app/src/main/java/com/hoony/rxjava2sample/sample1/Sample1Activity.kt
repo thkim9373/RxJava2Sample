7bf6c59ef3403ba4c013d03acf9ec9a72b8fd861
thkim9373 <thkim9373@gmail.com>
2020-10-02 23:10:20 +0900

    Save commit.

--- app/src/main/java/com/hoony/rxjava2sample/sample1/Sample1Activity.kt
+++ app/src/main/java/com/hoony/rxjava2sample/sample1/Sample1Activity.kt
@@ -1,6 +1,19 @@
 package com.hoony.rxjava2sample.sample1
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.hoony.rxjava2sample.R
+import com.hoony.rxjava2sample.databinding.ActivityLoginBinding
 
 class Sample1Activity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private val viewModel by viewModels<Sample1ViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
 }
---
+++ app/src/main/java/com/hoony/rxjava2sample/sample1/Sample1ViewModel.kt
@@ -0,0 +1,7 @@
+package com.hoony.rxjava2sample.sample1
+
+import androidx.lifecycle.ViewModel
+
+class Sample1ViewModel : ViewModel() {
+
+}
